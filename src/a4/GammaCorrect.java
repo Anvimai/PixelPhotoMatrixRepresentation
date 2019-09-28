cf154485e5f84bf073cd1e95eb44cc1496e236da
maej <maej@maej-pc.wireless-1x.unc.edu>
2019-09-28 16:54:19 -0400

    Gamma Correct is correct. When creating a new Color Pixel, be sure you initialize it in the right order. r,g,b.

--- src/a4/GammaCorrect.java
+++ src/a4/GammaCorrect.java
@@ -15,7 +15,15 @@ public class GammaCorrect implements PixelTransformation{
 	public Pixel transform(Pixel p) {
 		// TODO Auto-generated method stub
 		
-		return p.blend(p, Math.pow(p.getIntensity(), (1.0/gamma)));
+		
+		
+		Pixel pix = new ColorPixel(Math.pow(p.getRed(), 1.0/gamma),
+												Math.pow(p.getGreen(), 1.0/gamma),
+													Math.pow(p.getBlue(), 1.0/gamma));
+	
+		
+		
+		return pix;
 		
 
 	}
